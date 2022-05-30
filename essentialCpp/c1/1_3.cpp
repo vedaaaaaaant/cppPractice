fbcb29f351a3cd172048b8120ef0636dac5e8793
vp <vedantp42@gmail.com>
2022-05-30 17:32:23 +0800

    minor update

---
+++ essentialCpp/c1/1_3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int tries = 0;
+    cout << "Are you ready for try #"
+         << ++tries << "?\n"
+         << tries;
+    return 0;
+
+}
